e2a844797e018ad6b1057f5c0640224133825b29
terrotar <pedro_guedes93@hotmail.com>
2021-02-10 22:44:16 -0300

    Fixed some bugs in conversion of db in file 'test_main.py'. Fixed bugs of return in file 'crud.py'.

--- Pets/crud.py
+++ Pets/crud.py
@@ -7,15 +7,34 @@ from sql_Pets import models, schemas
 
 def list_pets_with_filter(db: Session, species: str = None):
     if species:
-        return db.query(models.Pet).filter(models.Pet.species == species)
+        all_pets_filter = db.query(models.Pet).filter(models.Pet.species == species).all()
+        results = []
+        for animal in all_pets_filter:
+            animal = {
+                "name": animal.name,
+                "species": animal.species,
+                "id": animal.id
+            }
+            results.append(animal)
+            return results
     else:
-        return db.query(models.Pet).all()
-    # Bug in return - not a valid List
+        results = []
+        all_pets = db.query(models.Pet).all()
+        for animal in all_pets:
+            animal = {
+                      "name": animal.name,
+                      "species": animal.species,
+                      "id": animal.id
+                     }
+            results.append(animal)
+        return results
+
 
 def create_pet(db: Session, new_pet: schemas.PetCreate):
     pet_db = models.Pet(**new_pet.dict())
     # Same as pet_db = models.Pet(name=new_pet.name, species=new_pet.species)
-    duplicate_pet = db.query(models.Pet).filter(models.Pet.name == pet_db.name, models.Pet.species == pet_db.species)
+    duplicate_pet = db.query(models.Pet).filter(
+        models.Pet.name == pet_db.name, models.Pet.species == pet_db.species)
     if duplicate_pet.count() >= 1:
         raise HTTPException(status_code=422, detail="Pet's already in database.")
     elif (pet_db.name == "string" or pet_db.species == "string"):
@@ -24,5 +43,8 @@ def create_pet(db: Session, new_pet: schemas.PetCreate):
         db.add(pet_db)
         db.commit()
         db.refresh(pet_db)
-        return pet_db
-        # Bug in returning de pet_db...
+        return {
+                "name": pet_db.name,
+                "species": pet_db.species,
+                "id": pet_db.id
+               }
--- Pets/test_main.py
+++ Pets/test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 
-from main import app, create_pet, PETS_LIST
+from main import app, create_pet
 
 
 client = TestClient(app)
@@ -9,18 +9,26 @@ client = TestClient(app)
 class TestPets():
     def test_create_pet_new_pet_name_specie_not_string(self):
         """
-        Check if new_pet["name"] and new_pet["specie"] "= "string"
+        Check if new_pet["name"] and new_pet["specie"] == "string"
         """
-        new_pet = {"name": "string", "species": "string"}
-        create_pet(new_pet)
-        assert create_pet(new_pet)
+        response = client.post('/pets', json={
+                                              "name": "string",
+                                              "species": "string"
+                                             })
+        assert response.status_code == 400
 
     def test_create_pet_new_pet_already_exists(self):
         """
         Check possibility of create a Pet that already exists..
-        If so, raise an HTTPException(status_code=422)
         """
-        response = client.post('/pets')
-        new_pet = PETS_LIST[-1]
-        PETS_LIST.append(new_pet)
+        client.post('/pets', json={
+                                    "name": "name",
+                                    "species": "species",
+                                    "id": "id"
+                                  })
+        response = client.post('/pets', json={
+                                              "name": "name",
+                                              "species": "species",
+                                              "id": "id"
+                                              })
         assert response.status_code == 422
